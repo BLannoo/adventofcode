fb944d097d342ed2d78c835c2b9808fec6872b36
BLannoo <brunolannoo@gmail.com>
2017-12-04 19:43:37 +0100

    initial test

---
+++ src/test/java/com/blannoo/adventofcode/day1/day1Test.java
@@ -0,0 +1,11 @@
+package com.blannoo.adventofcode.day1;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class day1Test {
+    @Test
+    public void test() throws Exception {
+        Assertions.assertThat(0).isEqualTo(0);
+    }
+}
