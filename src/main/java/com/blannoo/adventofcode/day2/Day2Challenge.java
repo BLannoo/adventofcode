153ad4c66be600e6a1568e9e13ba795b09755609
BLannoo <brunolannoo@gmail.com>
2017-12-04 22:44:15 +0100

    solution golden challenge day 2

--- src/main/java/com/blannoo/adventofcode/day2/Day2Challenge.java
+++ src/main/java/com/blannoo/adventofcode/day2/Day2Challenge.java
@@ -1,27 +1,61 @@
 package com.blannoo.adventofcode.day2;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.ToLongFunction;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 class Day2Challenge {
     long runSilverChallenge(String inputFileName) throws IOException {
+        return checkSum(inputFileName, this::differenceMinMax);
+    }
 
+    long runGoldenChallenge(String inputFileName) {
+        return checkSum(inputFileName, this::evenDivider);
+    }
+
+    private long checkSum(String inputFileName, ToLongFunction<String> lineEvaluator) {
         final InputStream stream = this.getClass().getResourceAsStream(inputFileName);
         final InputStreamReader reader = new InputStreamReader(stream);
         final BufferedReader buffered = new BufferedReader(reader);
 
         return buffered.lines()
-                .mapToLong(this::lineValue)
+                .mapToLong(lineEvaluator)
                 .sum();
     }
 
-    private long lineValue(String line) {
-        long max = Arrays.stream(line.split("\t"))
+    private long evenDivider(String line) {
+        List<Long> longs = lineToLongs(line).map(Long::parseLong).collect(Collectors.toList());
+        for (int i = 0; i < longs.size(); i++) {
+            long v1 = longs.get(i);
+            for (int j = 0; j < longs.size(); j++) {
+                if (i == j) {
+                    continue;
+                }
+                long v2 = longs.get(j);
+                if (v1 % v2 == 0) {
+                    return v1 / v2;
+                }
+            }
+        }
+        throw new IllegalArgumentException("No even divider present in line: " + line);
+    }
+
+    private long differenceMinMax(String line) {
+        long max = lineToLongs(line)
                 .mapToLong(Long::parseLong)
                 .max().orElse(0);
-        long min = Arrays.stream(line.split("\t"))
+        long min = lineToLongs(line)
                 .mapToLong(Long::parseLong)
                 .min().orElse(0);
         return max - min;
+    }
+
+    private Stream<String> lineToLongs(String line) {
+        return Arrays.stream(line.split("\t"));
     }
 }
--- src/test/java/com/blannoo/adventofcode/day2/Day2ChallengeTest.java
+++ src/test/java/com/blannoo/adventofcode/day2/Day2ChallengeTest.java
@@ -8,11 +8,21 @@ public class Day2ChallengeTest {
 
     @Test
     public void givenExample_WhenSilverRun_ThenReturn18() throws Exception {
-        Assertions.assertThat(day2Challenge.runSilverChallenge("/day2/exampleInput.txt")).isEqualTo(18);
+        Assertions.assertThat(day2Challenge.runSilverChallenge("/day2/silverExample.txt")).isEqualTo(18);
     }
 
     @Test
     public void givenChallenge_WhenSilverRun_ThenReturnSolution() throws Exception {
         Assertions.assertThat(day2Challenge.runSilverChallenge("/day2/challenge.txt")).isEqualTo(53460);
+    }
+
+    @Test
+    public void givenExample_WhenGoldenRun_ThenReturn9() throws Exception {
+        Assertions.assertThat(day2Challenge.runGoldenChallenge("/day2/goldenExample.txt")).isEqualTo(9);
+    }
+
+    @Test
+    public void givenChallenge_WhenGoldenRun_ThenReturnSolution() throws Exception {
+        Assertions.assertThat(day2Challenge.runGoldenChallenge("/day2/challenge.txt")).isEqualTo(282);
     }
 }
