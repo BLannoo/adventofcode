99344eaaffb148228485ffaf5fa9397b46a59b50
BLannoo <brunolannoo@gmail.com>
2017-12-04 19:50:38 +0100

    add test for other cases

--- src/test/java/com/blannoo/adventofcode/day1/day1Test.java
+++ src/test/java/com/blannoo/adventofcode/day1/day1Test.java
@@ -10,4 +10,19 @@ public class day1Test {
     public void Given1122_WhenRun_Then2() throws Exception {
         Assertions.assertThat(day1Challenge.run(1122)).isEqualTo(2);
     }
+
+    @Test
+    public void Given1111_WhenRun_Then4() throws Exception {
+        Assertions.assertThat(day1Challenge.run(1111)).isEqualTo(4);
+    }
+
+    @Test
+    public void Given1234_WhenRun_Then0() throws Exception {
+        Assertions.assertThat(day1Challenge.run(1234)).isEqualTo(0);
+    }
+
+    @Test
+    public void Given91212129_WhenRun_Then9() throws Exception {
+        Assertions.assertThat(day1Challenge.run(91212129)).isEqualTo(9);
+    }
 }
